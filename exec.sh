db6c63fcba72d225dbbe49210ff059b2903c93d8
dimgiagos44 <dimgiagos@gmail.com>
2022-02-07 20:48:10 +0200

    minor

--- exec.sh
+++ exec.sh
@@ -55,13 +55,13 @@ arrStep2=(${arrStep[1]//s/ })
 step=${arrStep2[0]}
 
 
-URL1="https://github.com/intel-iot-devkit/sample-videos/raw/master/head-pose-face-detection-female.mp4" #duration 2m 15s
-#URL1="https://im7.ezgif.com/tmp/ezgif-7-4365a891b9.mp4"
+#URL1="https://github.com/intel-iot-devkit/sample-videos/raw/master/head-pose-face-detection-female.mp4" #duration 2m 15s
+URL1="https://im3.ezgif.com/tmp/ezgif-3-b56610b863.mp4"
 URL2="https://raw.githubusercontent.com/intel-iot-devkit/sample-videos/master/bottle-detection.mp4" #duration 40s
 URL3="https://raw.githubusercontent.com/intel-iot-devkit/sample-videos/master/head-pose-face-detection-male.mp4" #duration 2m 14s
 URL4="https://im3.ezgif.com/tmp/ezgif-3-f0dedbb69f.mp4" #0-280 (1 frame / 15sec)
-URL5="https://im2.ezgif.com/tmp/ezgif-2-1087e7bdca.mp4" #0-500 (1 frame / 15sec)
-URL6="https://im3.ezgif.com/tmp/ezgif-3-83d935f259.mp4" #0-400 (1 frame / 10sec)
+URL5="https://im3.ezgif.com/tmp/ezgif-3-955cc84064.mp4" #0-500 (1 frame / 15sec)
+URL6="https://im3.ezgif.com/tmp/ezgif-3-955cc84064.mp4" #0-400 (1 frame / 10sec)
 URL7="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" #0-500 (1 frame / 5 sec)
 URL8="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
 
